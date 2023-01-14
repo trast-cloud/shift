9845aff2b67415cd0699ae1d68c9e4a27009575a
trast-cloud <77502979+trast-cloud@users.noreply.github.com>
2023-01-14 23:58:22 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int f = 0; f  < args.length; f++) {
+            System.out.println(args[f]);
+        }
+
+        int[] arrFile1 = new int[10];
+        FileReader readerFile1 = new FileReader("file1.txt");
+        Scanner scan1 = new Scanner(readerFile1);
+        int i = 0;
+        while (scan1.hasNextLine()) {
+            arrFile1[i] = scan1.nextInt();
+            i++;
+        }
+        readerFile1.close();
+
+        int[] arrFile2 = new int[10];
+        FileReader readerFile2 = new FileReader("file2.txt");
+        Scanner scan2 = new Scanner(readerFile2);
+        i = 0;
+        while (scan2.hasNextLine()) {
+            arrFile2[i] = scan2.nextInt();
+            i++;
+        }
+        readerFile2.close();
+
+        int[] arrWrite = Sort.StartSortInt(arrFile1, arrFile2, arrFile1.length, arrFile2.length, false);
+
+        FileWriter newRec = new FileWriter("sample.txt");
+        for (int a = 0; a < arrWrite.length; a++) {
+            newRec.write(arrWrite[a] + "\n");
+        }
+        newRec.close();
+    }
+}
---
+++ src/Sort.java
@@ -0,0 +1,40 @@
+public class Sort {
+    public static int[] StartSortInt(int[] ArrFile1, int[] ArrFile2, int lenFile1, int lenFile2, boolean bDesc){
+        int[] arrAsc    = new int[lenFile1 + lenFile2];
+        int[] arrDesc   = new int[lenFile1 + lenFile2];
+        int[] arrResult = new int[lenFile1 + lenFile2];
+        int a = 0, b = 0, r = 0, idx = 0, index = 0, iDesc = arrResult.length - 1;
+
+        while (a < ArrFile1.length && b < ArrFile2.length){
+            if (ArrFile1[a] <= ArrFile2[b])
+                arrAsc[r++] = ArrFile1[a++];
+            else
+                arrAsc[r++] = ArrFile2[b++];
+        }
+
+        while (a < lenFile1){
+            arrAsc[r++] = ArrFile1[a++];
+        }
+
+        while (b < lenFile2){
+            arrAsc[r++] = ArrFile2[b++];
+        }
+
+        if (bDesc) {
+            while (iDesc >= 0) {
+                arrDesc[idx++] = arrAsc[iDesc--];
+            }
+            for (int i = 0; i < arrResult.length; i++) {
+                arrResult[i] = arrDesc[index++];
+            }
+        }
+        else {
+            index = 0;
+            for (int i = 0; i < arrResult.length; i++) {
+                arrResult[i] = arrAsc[index++];
+            }
+        }
+
+        return arrResult;
+    }
+}
